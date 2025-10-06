9b2a5cd92e079056d86a7adbd1474e8da82e2c93
janeskil1525 <janeskil1525@gmail.com>
2025-10-06 17:32:47 +0200

    Table object started

---
+++ sql_gen/src/app/features/table-object/table-object.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-object.component',
+  imports: [],
+  templateUrl: './table-object.component.html',
+  styleUrl: './table-object.component.css'
+})
+export class TableObjectComponent {
+
+}
