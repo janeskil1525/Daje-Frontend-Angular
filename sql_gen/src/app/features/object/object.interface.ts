f84f91ccdb32d9d05fb57dcfed2e7db98e22164e
janeskil1525 <janeskil1525@gmail.com>
2025-10-13 16:20:43 +0200

    Tools object loaded

--- sql_gen/src/app/features/object/object.interface.ts
+++ sql_gen/src/app/features/object/object.interface.ts
@@ -1,5 +1,10 @@
 export interface ObjectInterface {
     tools_objects_pkey: number,
+    editnum: number,
+    insby:string,
+    insdatetime: string,
+    modby: string,
+    moddatetime: string,
     tools_version_fkey: number,
     type: string,
     name: string,
---
+++ sql_gen/src/app/features/object/object.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, inject } from '@angular/core';
+import { Observable }  from 'rxjs';
+import { LocalStorageService } from '../../core/localstorage/local-storage.service';
+import { ResponseInterface } from '../../core/response/response.interface';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ObjectService {
+  private localkey: string = '';
+  private http = inject(HttpClient);
+
+  constructor(private localstorage: LocalStorageService) {}
+
+  getRecord(tools_objects_pkey:number): Observable<ResponseInterface[]> {
+      this.localkey = this.localstorage.getItem('X-Token-Check')!;
+  
+      return this.http.get<ResponseInterface[]>('http://localhost/tools/api/v1/object' + tools_objects_pkey,{
+        headers:{
+          'X-Token-Check': this.localkey
+          //''
+        }
+      });
+  }
+}
